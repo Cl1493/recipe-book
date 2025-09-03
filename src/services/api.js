f5d52409922ed441f3aaf3e892cb641839d05a9f
Cl1493 <formaciondecarla@gmail.com>
2025-09-03 13:17:48 +0200

    getRecipes-working

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,31 @@
 export async function getRecipes() {
-    const response = await fetch("../../../public/recipes.json");
-    return response.json();
-}
+    const url = "../../../recipes.json";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+            }
+        const result = await response.json();
+        console.log(result);
+        return result;
+    } catch (error) {
+        console.log(error.message);
+    }
+};
 
 export async function getRecipesById(id) {
-    const response = await fetch("../../../public/recipes.json");
-    const data = await response.json();
-    return data.find(recipe => recipe.id === id);
-}
+    const url = "../../../recipes.json";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`); 
+        }
+        const data = await response.json();
+        console.log(data);
+       const result = data.find((recipe) => recipe.id === id);
+        console.log(result);
+        return result;
+    } catch (error) {
+        console.log(error.message);
+    }
+};
